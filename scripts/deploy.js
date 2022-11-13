771d536fcbb4da412eb92a31d7d1f00e837edce7
Julien Bourseau <Julien.Bourseau@grenoble-inp.org>
2022-11-13 01:06:11 +0100

    Deploying with hardhat

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,31 +1,60 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+// imports necessary, we import ethers through hardhat
+// run package to run any hardhat task
+const { ethers, run, network } = require("hardhat");
 
+// async main function to deplay
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+    /* Hardhat understands that SimpleStorage is already compiled and in contracts/ */
+    const SimpleStorageFactory = await ethers.getContractFactory(
+        "SimpleStorage"
+    );
+    console.log("Deploying the contract, please wait...");
+    /* By default, hardhat is deploying to local Ethereum hardhat network */
+    const SimpleStorage = await SimpleStorageFactory.deploy();
+    await SimpleStorage.deployed();
+    console.log("Contract adress is: ", SimpleStorage.address);
 
-  const lockedAmount = hre.ethers.utils.parseEther("1");
+    // verifying the contract when deploying IF it's a live network
+    // console.log("network config = ", network.config);
+    if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+        // wait a bit so etherscan can see the contract deployed
+        console.log("Wait 5 blocks before verifying..");
+        await SimpleStorage.deployTransaction.wait(5);
+        await verify(SimpleStorage.address, []); // don't forget the await!
+    }
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    // see current value...
+    const currentValue = await SimpleStorage.retrieve();
+    console.log("The current value is ", currentValue);
 
-  await lock.deployed();
-
-  console.log(
-    `Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+    // and change it:
+    const transactionResponse = await SimpleStorage.store(41);
+    await transactionResponse.wait(1);
+    const updatedValue = await SimpleStorage.retrieve();
+    console.log("And now, the value is", updatedValue);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+// function to verify contract
+async function verify(contractAdress, args) {
+    console.log("Verifying the contract, please wait...");
+    try {
+        await run("verify:verify", {
+            address: contractAdress,
+            constructorArguments: args,
+        });
+        // if etherscan already verified it automatically
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+            console.log("Already verified");
+        } else {
+            console.log(e);
+        }
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
